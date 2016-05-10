52a5d6c5c8a8503fecdce29255893686a4c7fe11
Travis-CI <wolfram+travis-for-crewmeister@crewmeister.com>
2016-05-10 16:01:08 +0000

    Travis deployed 'master' - Tue May 10 16:01:08 UTC 2016

--- index.js
+++ index.js
@@ -1,7 +1,42 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+'use strict';
+
+var _index = require('../src/index');
+
+var options = {
+  maxWait: 10000,
+  desiredAccuracy: 70
+};
+document.getElementById('start').innerHTML = new Date() + '\nCalled with options:\n' + JSON.stringify(options, null, 4);
+
+function stringifyObj(coords) {
+  var obj = {};
+  for (var key in coords) {
+    obj[key] = coords[key];
+  }
+  return JSON.stringify(obj, null, 4);
+}
+
+function inProgress(pos) {
+  document.getElementById('progress').innerHTML += new Date() + '\n' + stringifyObj(pos.coords) + '\n';
+}
+
+function onSuccess(pos) {
+  document.getElementById('result').innerHTML = new Date() + '\n' + stringifyObj(pos.coords);
+}
+
+function onError(err) {
+  document.getElementById('error').innerHTML = stringifyObj(err);
+}
+(0, _index.accurateCurrentPosition)(onSuccess, onError, inProgress, options);
+
+},{"../src/index":2}],2:[function(require,module,exports){
 "use strict";
 
-navigator.geolocation.getAccurateCurrentPosition = function (geolocationSuccess, geolocationError, geoprogress, options) {
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+var accurateCurrentPosition = exports.accurateCurrentPosition = function accurateCurrentPosition(geolocationSuccess, geolocationError, geoprogress, options) {
     var lastCheckedPosition,
         locationEventCount = 0,
         watchID,
