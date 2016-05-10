cd0ba0b282fdbc4228b997e6abc393c9fa00e235
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-05-10 18:03:10 +0200

    More ES6ifying.

--- example/index.js
+++ example/index.js
@@ -1,28 +1,32 @@
 import { accurateCurrentPosition } from '../src/index';
 
-var options = {
+const options = {
   maxWait: 10000,
   desiredAccuracy: 70,
 };
-document.getElementById('start').innerHTML = new Date() + '\nCalled with options:\n' + JSON.stringify(options, null, 4);
+document.getElementById('start').innerHTML = `${new Date()}
+Called with options:
+${JSON.stringify(options, null, 4)}
+`;
 
-function stringifyObj(coords) {
-  var obj = {};
-  for (var key in coords) {
+const stringifyObj = (coords) => {
+  let obj = {};
+  for (let key in coords) {
     obj[key] = coords[key];
   }
   return JSON.stringify(obj, null, 4);
-}
+};
 
-function inProgress(pos) {
+const inProgress = (pos) => {
   document.getElementById('progress').innerHTML += new Date() + '\n' + stringifyObj(pos.coords) + '\n';
-}
+};
 
-function onSuccess(pos) {
+const onSuccess = (pos) => {
   document.getElementById('result').innerHTML = new Date() + '\n' + stringifyObj(pos.coords);
-}
+};
 
-function onError(err) {
+const onError = (err) => {
   document.getElementById('error').innerHTML = stringifyObj(err);
-}
+};
+
 accurateCurrentPosition(onSuccess, onError, inProgress, options);
