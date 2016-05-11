eae51d854b4b9b450bd79b72fbeba6e7d57b467d
Wolfram Kriesing <wolfram+github@kriesing.de>
2016-05-11 09:09:36 +0200

    Test for the timeout and not finding the right accuracy.

--- src/index.spec.js
+++ src/index.spec.js
@@ -5,6 +5,7 @@ import {
   rejected,
   assertThat,
   equalTo,
+  hasProperty,
 } from 'hamjest';
 
 import { accurateCurrentPosition, _accurateCurrentPosition } from './index';
@@ -41,14 +42,34 @@ describe('`accurateCurrentPosition()`', () => {
   });
 
   it('returns when the desired accuracy was returned', () => {
+    const desiredAccuracy = 42;
     navigator.geolocation.watchPosition = (onSuccess, _, {}) => {
       onSuccess({ coords: { accuracy: 1000 } });
-      onSuccess({ coords: { accuracy: 42 } });
+      onSuccess({ coords: { accuracy: 44 } });
+      onSuccess({ coords: { accuracy: 43 } });
+      onSuccess({ coords: { accuracy: desiredAccuracy } });
     };
 
-    const options = { desiredAccuracy: 42 };
+    const options = { desiredAccuracy };
     return promiseThat(accurateCurrentPosition(options),
-      isFulfilledWith({ coords: { accuracy: 42 } })
+      isFulfilledWith({ coords: { accuracy: desiredAccuracy } })
+    );
+  });
+
+  it('when desiredAccuracy does not occur within timeout, returns the LAST position', () => {
+    const finalAccuracy = 42;
+    const desiredAccuracy = finalAccuracy - 1;
+    navigator.geolocation.watchPosition = (onSuccess, _, {}) => {
+      onSuccess({ coords: { accuracy: 1000 } });
+      onSuccess({ coords: { accuracy: 44 } });
+      onSuccess({ coords: { accuracy: 43 } });
+      onSuccess({ coords: { accuracy: finalAccuracy } });
+    };
+
+    const maxWait = 1;
+    const options = { maxWait, desiredAccuracy };
+    return promiseThat(accurateCurrentPosition(options),
+      isFulfilledWith({ coords: { accuracy: finalAccuracy } })
     );
   });
   
